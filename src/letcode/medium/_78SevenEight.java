125a27e5ed788676a2490bd6f3874a8beaad63b2
HerculesCyc <herculescyc@163.com>
2020-07-18 14:59:54 +0800

    日常提交

---
+++ src/letcode/medium/_78SevenEight.java
@@ -0,0 +1,64 @@
+package letcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Leetcode
+ * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ * 说明：解集不能包含重复的子集
+ *
+ * @author : CaiYongcheng
+ * @date : 2020-07-18 11:17
+ **/
+public class _78SevenEight {
+
+    private static ArrayList<List<Integer>> lists;
+    private static ArrayList<Integer> list;
+    private static int[] data;
+
+    private static void dfs(int startIndex){
+        if (startIndex >= data.length){
+            return;
+        }
+        for (; startIndex<data.length; ++startIndex){
+            list.add(data[startIndex]);
+            lists.add(new ArrayList<>(list));
+            dfs(startIndex+1);
+            list.remove(list.size()-1);
+        }
+    }
+
+    /**
+     * 示例:
+     * 输入: nums = [1,2,3]
+     * 输出:
+     * [
+     *   [3],
+     *   [1],
+     *   [2],
+     *   [1,2,3],
+     *   [1,3],
+     *   [2,3],
+     *   [1,2],
+     *   []
+     * ]
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> subsets(int[] nums) {
+        list = new ArrayList<>(nums.length);
+        lists = new ArrayList<List<Integer>>(1<<nums.length);
+        lists.add(new ArrayList<>(list));
+        data = nums;
+        dfs(0);
+        return lists;
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> subsets = subsets(new int[]{1, 2, 3});
+        for (List<Integer> subset : subsets) {
+            System.out.println(subset);
+        }
+    }
+}
---
+++ src/letcode/medium/_79SevenNine.java
@@ -0,0 +1,85 @@
+package letcode.medium;
+
+/**
+ * Leetcode
+ * 给定一个二维网格和一个单词，找出该单词是否存在于网格中。
+ * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。
+ * 同一个单元格内的字母不允许被重复使用。   
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/word-search 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author : CaiYongcheng
+ * @date : 2020-07-18 11:38
+ **/
+public class _79SevenNine {
+
+    private boolean[][] use;
+    private char[][] data;
+    private char[] patten;
+    private int[][] moveStep = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    private boolean dfs(int y, int x, int index){
+        if (index == patten.length){
+            return true;
+        }
+        for (int i=0; i<4; ++i){
+            int ny = y + moveStep[i][0];
+            int nx = x + moveStep[i][1];
+            if (ny > -1 && ny < use.length
+                    && nx > -1 && nx < use[ny].length
+                    && data[ny][nx] == patten[index] && !use[ny][nx]){
+                use[ny][nx] = true;
+                if (dfs(ny, nx, index+1)){
+                    return true;
+                }
+                use[ny][nx] = false;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 示例:
+     * board =
+     * [
+     *   ['A','B','C','E'],
+     *   ['S','F','C','S'],
+     *   ['A','D','E','E']
+     * ]
+     * 给定 word = "ABCCED", 返回 true
+     * 给定 word = "SEE", 返回 true
+     * 给定 word = "ABCB", 返回 false
+     * @param board
+     * @param word
+     * @return
+     */
+    public boolean exist(char[][] board, String word) {
+        data = board;
+        use = new boolean[board.length][board[0].length];
+        patten = word.toCharArray();
+        for (int y=0; y<board.length; ++y){
+            for (int x=0; x<board[y].length; ++x){
+                if (data[y][x] == patten[0]){
+                    use[y][x] = true;
+                    if (dfs(y, x, 1)){
+                        return true;
+                    }
+                    use[y][x] = false;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {
+                {'A','B','C','E'} ,
+                {'S','F','C','S'},
+                {'A','D','E','E'}
+        };
+        String word = "ABCB";
+        System.out.println(new _79SevenNine().exist(board, word));
+    }
+
+
+}
