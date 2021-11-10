1e3ed19c930126e40ff534e2bd1a1e786aa02287
蔡永程 <17740964843@163.com>
2021-11-10 09:28:10 +0800

    日常提交

---
+++ src/letcode/normal/easy/_496FourHundredNinetySix.java
@@ -0,0 +1,111 @@
+/*
+ * 版权所有（c）<2021><蔡永程>
+ *
+ * 反996许可证版本1.0
+ *
+ * 在符合下列条件的情况下，特此免费向任何得到本授权作品的副本（包括源代码、文件和/或相关内容，以
+ * 下统称为“授权作品”）的个人和法人实体授权：被授权个人或法人实体有权以任何目的处置授权作品，包括
+ * 但不限于使用、复制，修改，衍生利用、散布，发布和再许可：
+ *
+ * 1. 个人或法人实体必须在许可作品的每个再散布或衍生副本上包含以上版权声明和本许可证，不得自行修
+ * 改。
+ * 2. 个人或法人实体必须严格遵守与个人实际所在地或个人出生地或归化地、或法人实体注册地或经营地（
+ * 以较严格者为准）的司法管辖区所有适用的与劳动和就业相关法律、法规、规则和标准。如果该司法管辖区
+ * 没有此类法律、法规、规章和标准或其法律、法规、规章和标准不可执行，则个人或法人实体必须遵守国际
+ * 劳工标准的核心公约。
+ * 3. 个人或法人不得以任何方式诱导、暗示或强迫其全职或兼职员工或其独立承包人以口头或书面形式同意
+ * 直接或间接限制、削弱或放弃其所拥有的，受相关与劳动和就业有关的法律、法规、规则和标准保护的权利
+ * 或补救措施，无论该等书面或口头协议是否被该司法管辖区的法律所承认，该等个人或法人实体也不得以任
+ * 何方法限制其雇员或独立承包人向版权持有人或监督许可证合规情况的有关当局报告或投诉上述违反许可证
+ * 的行为的权利。
+ *
+ * 该授权作品是"按原样"提供，不做任何明示或暗示的保证，包括但不限于对适销性、特定用途适用性和非侵
+ * 权性的保证。在任何情况下，无论是在合同诉讼、侵权诉讼或其他诉讼中，版权持有人均不承担因本软件或
+ * 本软件的使用或其他交易而产生、引起或与之相关的任何索赔、损害或其他责任。
+ */
+
+package letcode.normal.easy;
+
+import letcode.utils.FormatUtils;
+
+import java.util.Stack;
+
+/**
+ * 给你两个 没有重复元素 的数组nums1 和nums2，其中nums1是nums2的子集。
+ * 请你找出 nums1中每个元素在nums2中的下一个比其大的值。
+ * nums1中数字x的下一个更大元素是指x在nums2中对应位置的右边的第一个比x大的元素。如果不存在，对应位置输出 -1 。
+ * 来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/next-greater-element-i 著作权归领扣网络所有。
+ * <p>
+ * 1 <= nums1.length <= nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 104
+ * nums1和nums2中所有整数 互不相同
+ * nums1 中的所有整数同样出现在 nums2 中
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/next-greater-element-i
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * <p>
+ * 商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author CaiYongcheng
+ * @date 2021-10-26 10:12
+ **/
+public class _496FourHundredNinetySix {
+
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        /*
+         * 需要一个数据结构 保存元素-》下一个再右侧，且比他大的元素位置。
+         * 因为数据范围已经给定，比较小，所以使用数组。
+         * 使用单调栈
+         */
+        int[] ans = new int[nums1.length];
+        int[] valToIndex = new int[10001];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(nums2[0]);
+        for (int index = 1; index < nums2.length; index++) {
+            while (!stack.isEmpty() && nums2[index] > stack.peek()) {
+                valToIndex[stack.pop()] = nums2[index];
+            }
+            stack.push(nums2[index]);
+        }
+        while (!stack.isEmpty()) {
+            valToIndex[stack.pop()] = -1;
+        }
+        for (int index = 0; index < nums1.length; index++) {
+            ans[index] = valToIndex[nums1[index]];
+        }
+        return ans;
+    }
+
+
+    /**
+     * 示例 1:
+     * <p>
+     * 输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
+     * 输出: [-1,3,-1]
+     * 解释:
+     * 对于 num1 中的数字 4 ，你无法在第二个数组中找到下一个更大的数字，因此输出 -1 。
+     * 对于 num1 中的数字 1 ，第二个数组中数字1右边的下一个较大数字是 3 。
+     * 对于 num1 中的数字 2 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+     * 示例 2:
+     * <p>
+     * 输入: nums1 = [2,4], nums2 = [1,2,3,4].
+     * 输出: [3,-1]
+     * 解释:
+     * 对于 num1 中的数字 2 ，第二个数组中的下一个较大数字是 3 。
+     * 对于 num1 中的数字 4 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/next-greater-element-i
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(FormatUtils.formatArray(new _496FourHundredNinetySix().nextGreaterElement(
+                new int[]{4, 1, 2},
+                new int[]{1, 3, 4, 2}
+        )));
+    }
+
+}
