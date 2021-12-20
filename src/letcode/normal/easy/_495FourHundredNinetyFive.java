79796d1b01a6e7fc30cb107bdc30c2940b0a17f2
蔡永程 <17740964843@163.com>
2021-12-20 15:05:18 +0800

    日常提交

--- src/letcode/normal/easy/_495FourHundredNinetyFive.java
+++ src/letcode/normal/easy/_495FourHundredNinetyFive.java
@@ -26,6 +26,9 @@
 
 package letcode.normal.easy;
 
+import java.io.*;
+import java.util.Arrays;
+
 /**
  * 在《英雄联盟》的世界中，有一个叫 “提莫” 的英雄。他的攻击可以让敌方英雄艾希（编者注：寒冰射手）进入中毒状态。  当提莫攻击艾希，艾希的中毒状态正好持续 duration 秒。  正式地讲，提莫在 t 发起发起攻击意味着艾希在时间区间 [t, t + duration - 1]（含 t 和 t + duration - 1）处于中毒状态。如果提莫在中毒影响结束 前 再次攻击，中毒状态计时器将会 重置 ，在新的攻击之后，中毒影响将会在 duration 秒后结束。  给你一个 非递减 的整数数组 timeSeries ，其中 timeSeries[i] 表示提莫在 timeSeries[i] 秒时对艾希发起攻击，以及一个表示中毒持续时间的整数 duration 。  返回艾希处于中毒状态的 总 秒数。  来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/teemo-attacking 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
@@ -76,14 +79,13 @@ public class _495FourHundredNinetyFive {
      *
      * @param args
      */
-    public static void main(String[] args) {
-        System.out.println(new _495FourHundredNinetyFive().findPoisonedDuration(
-                new int[]{1, 2},
-                2
-        ));
-        for (int i = 0; i < 1000; i++) {
-            System.out.print(i + i + 1);
-            System.out.print(",");
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader =
+                new BufferedReader(new FileReader(new File("C:\\Users\\10761\\Downloads\\testDownload\\202108096661249913934110846.txt")));
+        String str = null;
+        System.out.println("insert into YLBZXXPT_MZMTB(BZMLDM, BZDLDM,BZMC,)");
+        while ((str = bufferedReader.readLine()) != null) {
+
         }
     }
 
