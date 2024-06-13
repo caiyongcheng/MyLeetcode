50aba0b1c3576ba2c8b0bd890fd1783fb1bc4c7a
蔡永程 <17740964843@163.com>
2024-06-13 15:48:44 +0800

    feat<>: question of the day

--- src/main/java/work/generic/GenericCodeUtil.java
+++ src/main/java/work/generic/GenericCodeUtil.java
@@ -15,25 +15,25 @@ public class GenericCodeUtil {
 
     public static void init() {
         LineStrConverter.init(
-                "lab_instrument_item_alarm_ref",
-                "仪器项目危急值参考",
-                "ins_id|ins_id|仪器ID|bigint(8)|not null\n" +
-                        "item_code|item_code|项目代码|nvarchar(8000)|not null\n" +
-                        "ref_value_seq_no|ref_value_seq_no|参考值序号|bigint(8)|not null\n" +
-                        "ref_type|ref_type|参考值关联类型|int(4)|null\n" +
-                        "sex|sex|性别|int(4)|null\n" +
-                        "up_age|up_age|上限年龄|int(4)|null\n" +
-                        "low_age|low_age|下限年龄|int(4)|null\n" +
-                        "sample_type|sample_type|样本类型|int(4)|null\n" +
-                        "up_limit|up_limit|上限参考值|decimal(17)|null\n" +
-                        "low_limit|low_limit|下限参考值|decimal(17)|null\n" +
-                        "char_ref|char_ref|参考值|nvarchar(8000)|null\n" +
-                        "enable_flag|enable_flag|是否使用|int(4)|null\n" +
-                        "creator|creator|记录创建人|bigint(8)|null\n" +
-                        "create_time|create_time|记录创建时间|datetime(8)|null\n" +
-                        "last_modifier|last_modifier|最后更新人|bigint(8)|null\n" +
-                        "last_modify_time|last_modify_time|最后更新时间|datetime(8)|null\n" +
-                        "alarm_ref_id|alarm_ref_id|无说明|bigint(8)|not null\n"
+                "lab_alarm_info",
+                "检验结果危急值信息表",
+                "exam_result_id|exam_result_id|检验结果表主键|bigint(8)|not null\n" +
+                        "sample_detail_id|sample_detail_id|检验申请明细主键|bigint(8)|null\n" +
+                        "sample_info_id|sample_info_id|申请单表主键|bigint(8)|not null\n" +
+                        "ins_id|ins_id|仪器主键|bigint(8)|not null\n" +
+                        "sample_date|sample_date|申请日期|datetime(8)|not null\n" +
+                        "operate_time|operate_time|结果填写日期|datetime(8)|null\n" +
+                        "sample_no|sample_no|申请编号|bigint(8)|not null\n" +
+                        "ad_number|ad_number|门诊、住院、病历号|nvarchar(8000)|not null\n" +
+                        "card_no|card_no|卡号|nvarchar(8000)|null\n" +
+                        "pat_name|pat_name|病人姓名|nvarchar(8000)|not null\n" +
+                        "pat_sex|pat_sex|病人性别|int(4)|null\n" +
+                        "dept_name|dept_name|科室名称|nvarchar(8000)|null\n" +
+                        "item_name|item_name|检验项目名称|nvarchar(8000)|null\n" +
+                        "item_value|item_value|检验值|nvarchar(8000)|null\n" +
+                        "origin_value|origin_value|原始值|nvarchar(8000)|null\n" +
+                        "adjust_value|adjust_value|调整值|nvarchar(8000)|null\n" +
+                        "lab_alarm_info_id|lab_alarm_info_id|id主键|bigint(8)|not null\n"
         );
     }
 
