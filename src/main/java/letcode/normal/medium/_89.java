ae6d402954b69311ef1dca5aaea4532a4b65a34c
蔡永程 <17740964843@163.com>
2024-07-19 09:35:26 +0800

    feat(test-utils): add and modify test utilities
    - Add a new utility class `TestUtil` for common testing operations.
    - change input and output test case util

--- src/main/java/letcode/normal/medium/_89.java
+++ src/main/java/letcode/normal/medium/_89.java
@@ -26,6 +26,9 @@
 
 package letcode.normal.medium;
 
+import letcode.utils.TestUtil;
+
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -100,8 +103,27 @@ public class _89 {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println(new _89().grayCode(3));
+
+        BigDecimal price = new BigDecimal("0.034333");
+        int count = 1;
+        while (count <= 200) {
+            BigDecimal total = price.multiply(new BigDecimal(count));
+            if (total.toString().charAt(4) == '5') {
+                System.out.println(total);
+                System.out.println(count);
+            }
+            ++count;
+        }
+
+
+        TestUtil.test(_89.class, "输入：n = 2 输出：[0,1,3,2] 解释： [0,1,3,2] 的二进制表示是 [00,01,11,10] 。" +
+                " - 00 和 01 有一位不同 - 01 和 11 有一位不同 - 11 和 10 有一位不同 - 10 和 00 有一位不同 [0,2,3,1] " +
+                "也是一个有效的格雷码序列，其二进制表示是 [00,10,11,01] 。 - 00 和 10 有一位不同 -" +
+                " 10 和 11 有一位不同 - 11 和 01 有一位不同 - 01 和 00 有一位不同 示例 2：\n" +
+                "输入：n = 1 输出：[0,1]\n" +
+                "[0,1] [0,1,3,2] [0,1,3,2,7,6,5,4] [0,1,3,2,7,6,5,4,15,14,13,12,11,10,9,8]");
     }
+
 
 
 }
