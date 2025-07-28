aae436dad0b8b856b809918ea123057df8a64809
蔡永程 <17740964843@163.com>
2025-07-28 15:02:00 +0800

    perf<normal-medium>: question of the day

--- src/main/java/letcode/normal/medium/_2044.java
+++ src/main/java/letcode/normal/medium/_2044.java
@@ -26,6 +26,8 @@
 
 package letcode.normal.medium;
 
+import letcode.utils.TestUtil;
+
 /**
  * 给你一个整数数组 nums ，请你找出 nums 子集 按位或 可能得到的 最大值 ，并返回按位或能得到最大值的 不同非空子集的数目 。  
  * 如果数组 a 可以由数组 b 删除一些元素（或不删除）得到，则认为数组 a 是数组 b 的一个 子集 。如果选中的元素下标位置不一样，则认为两个子集 不同 。  
@@ -57,7 +59,7 @@ public class _2044 {
      * @param pos pos表示当前处理的位
      * @param orVal orVal是之前处理位的或值
      */
-    public void dfs(int pos, int orVal) {
+    private void dfs(int pos, int orVal) {
         if (pos == nums.length) {
             if (orVal > maxOr) {
                 maxOr = orVal;
@@ -69,6 +71,37 @@ public class _2044 {
         }
         dfs(pos + 1, orVal | nums[pos]);
         dfs(pos + 1, orVal);
+    }
+
+
+    /**
+     * Example 1:
+     *
+     * Input: nums = [3,1]
+     * Output: 2
+     * Explanation: The maximum possible bitwise OR of a subset is 3. There are 2 subsets with a bitwise OR of 3:
+     * - [3]
+     * - [3,1]
+     * Example 2:
+     *
+     * Input: nums = [2,2,2]
+     * Output: 7
+     * Explanation: All non-empty subsets of [2,2,2] have a bitwise OR of 2. There are 23 - 1 = 7 total subsets.
+     * Example 3:
+     *
+     * Input: nums = [3,2,1,5]
+     * Output: 6
+     * Explanation: The maximum possible bitwise OR of a subset is 7. There are 6 subsets with a bitwise OR of 7:
+     * - [3,5]
+     * - [3,1,5]
+     * - [3,2,5]
+     * - [3,2,1,5]
+     * - [2,5]
+     * - [2,1,5]
+     * @param args
+     */
+    public static void main(String[] args) {
+        TestUtil.test();
     }
 
 
