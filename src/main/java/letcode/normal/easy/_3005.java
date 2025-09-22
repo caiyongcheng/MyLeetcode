0ac15fe9228c3b17feafd4219bcc494ff0cdb0f3
蔡永程 <17740964843@163.com>
2025-09-22 11:08:37 +0800

    feat<easy>: question of the day

---
+++ src/main/java/letcode/normal/easy/_3005.java
@@ -0,0 +1,54 @@
+package letcode.normal.easy;
+
+import letcode.utils.TestUtil;
+
+/**
+ * You are given an array nums consisting of positive integers.
+ * Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+ * The frequency of an element is the number of occurrences of that element in the array.
+ *
+ * @author 蔡永程
+ * @version 1.0.0
+ * @since 2025-09-22 11:01
+ */
+public class _3005 {
+
+    public int maxFrequencyElements(int[] nums) {
+        int[] num2FrequencyMap = new int[101];
+        int maxFrequency = 0;
+        int ans = 0;
+
+        for (int num : nums) {
+            num2FrequencyMap[num]++;
+            if (num2FrequencyMap[num] > maxFrequency) {
+                maxFrequency = num2FrequencyMap[num];
+                ans = num2FrequencyMap[num];
+            } else if (num2FrequencyMap[num] == maxFrequency) {
+                ans += num2FrequencyMap[num];
+            }
+        }
+
+        return ans;
+    }
+
+
+    /**
+     * Example 1:
+     *
+     * Input: nums = [1,2,2,3,1,4]
+     * Output: 4
+     * Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
+     * So the number of elements in the array with maximum frequency is 4.
+     * Example 2:
+     *
+     * Input: nums = [1,2,3,4,5]
+     * Output: 5
+     * Explanation: All elements of the array have a frequency of 1 which is the maximum.
+     * So the number of elements in the array with maximum frequency is 5.
+     * @param args
+     */
+    public static void main(String[] args) {
+        TestUtil.test();
+    }
+
+}
