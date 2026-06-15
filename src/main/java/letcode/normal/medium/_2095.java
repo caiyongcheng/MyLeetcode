24b235f5ceccb4540d2842683075693ac389c6df
caiyongcheng <hercules.cyc@gmail.com>
2026-06-15 17:38:41 +0800

    feat<hard>: question of the day

--- src/main/java/letcode/normal/medium/_2095.java
+++ src/main/java/letcode/normal/medium/_2095.java
@@ -33,6 +33,25 @@ import letcode.utils.ListNode;
 public class _2095 {
 
     public ListNode deleteMiddle(ListNode head) {
+
+        /**
+         *         if (head.next == null) {
+         *             return null;
+         *         }
+         *         ListNode slow = head;
+         *         ListNode fast = head;
+         *         ListNode pre = null;
+         *
+         *         while (fast != null && fast.next != null) {
+         *             fast = fast.next.next;
+         *             pre = slow;
+         *             slow = slow.next;
+         *         }
+         *
+         *         pre.next = pre.next.next;
+         *         return head;
+         */
+
         if (head == null || head.next == null) {
             return null;
         }
