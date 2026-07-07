d436359498c9786fe61a6e562b8bcf1068d36ddc
caiyongcheng <hercules.cyc@gmail.com>
2026-07-07 09:17:50 +0800

    feat<medium>: question of the day

--- src/main/java/letcode/normal/medium/_1288.java
+++ src/main/java/letcode/normal/medium/_1288.java
@@ -1,43 +1,41 @@
-/*
- * 版权所有（c）<2021><蔡永程>
- *
- * 反996许可证版本1.0
- *
- * 在符合下列条件的情况下，特此免费向任何得到本授权作品的副本（包括源代码、文件和/或相关内容，以
- * 下统称为“授权作品”）的个人和法人实体授权：被授权个人或法人实体有权以任何目的处置授权作品，包括
- * 但不限于使用、复制，修改，衍生利用、散布，发布和再许可：
- *
- * 1. 个人或法人实体必须在许可作品的每个再散布或衍生副本上包含以上版权声明和本许可证，不得自行修
- * 改。
- * 2. 个人或法人实体必须严格遵守与个人实际所在地或个人出生地或归化地、或法人实体注册地或经营地（
- * 以较严格者为准）的司法管辖区所有适用的与劳动和就业相关法律、法规、规则和标准。如果该司法管辖区
- * 没有此类法律、法规、规章和标准或其法律、法规、规章和标准不可执行，则个人或法人实体必须遵守国际
- * 劳工标准的核心公约。
- * 3. 个人或法人不得以任何方式诱导、暗示或强迫其全职或兼职员工或其独立承包人以口头或书面形式同意
- * 直接或间接限制、削弱或放弃其所拥有的，受相关与劳动和就业有关的法律、法规、规则和标准保护的权利
- * 或补救措施，无论该等书面或口头协议是否被该司法管辖区的法律所承认，该等个人或法人实体也不得以任
- * 何方法限制其雇员或独立承包人向版权持有人或监督许可证合规情况的有关当局报告或投诉上述违反许可证
- * 的行为的权利。
- *
- * 该授权作品是"按原样"提供，不做任何明示或暗示的保证，包括但不限于对适销性、特定用途适用性和非侵
- * 权性的保证。在任何情况下，无论是在合同诉讼、侵权诉讼或其他诉讼中，版权持有人均不承担因本软件或
- * 本软件的使用或其他交易而产生、引起或与之相关的任何索赔、损害或其他责任。
- */
-
 package letcode.normal.medium;
+
 import java.util.Arrays;
 
-
 /**
- * 给你一个区间列表，请你删除列表中被其他区间所覆盖的区间。  只有当c <= a且b <= d时，我们才认为区间[a,b) 被区间[c,d) 覆盖。  
- * 在完成所有删除操作后，请你返回列表中剩余区间的数目。  
- * 来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/remove-covered-intervals 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 1288. Remove Covered Intervals
+ * Difficulty: Medium
+ * Link: https://leetcode.cn/problems/remove-covered-intervals/
  *
- * @author CaiYongcheng
- * @since 2021-04-25 14:55
- **/
+ * Given an array intervals where intervals[i] = [l i , r i ] represent the interval [l i , r i ) ,
+ * remove all intervals that are covered by another interval in the list.
+ *
+ * The interval [a, b) is covered by the interval [c, d) if and only if c <= a and b <= d .
+ *
+ * Return the number of remaining intervals .
+ *
+ * Example 1:
+ *
+ * Input: intervals = [[1,4],[3,6],[2,8]]
+ * Output: 2
+ * Explanation: Interval [3,6] is covered by [2,8], therefore it is removed.
+ *
+ * Example 2:
+ *
+ * Input: intervals = [[1,4],[2,3]]
+ * Output: 1
+ *
+ * Constraints:
+ *
+ * - 1 <= intervals.length <= 1000
+ *
+ * - intervals[i].length == 2
+ *
+ * - 0 <= l i < r i <= 10 5
+ *
+ * - All the given intervals are unique .
+ */
 public class _1288 {
-
 
     class Section {
         int s;
