08232791e640a8baaf38f0b2df2542e95718b2a2
caiyongcheng <hercules.cyc@gmail.com>
2026-08-18 14:34:01 +0800

    feat<medium>: question of the day

---
+++ src/main/java/letcode/normal/medium/_840.java
@@ -0,0 +1,110 @@
+package letcode.normal.medium;
+
+/**
+ * 840. Magic Squares In Grid
+ * Difficulty: Medium
+ * Link: https://leetcode.cn/problems/magic-squares-in-grid/
+ * <p>
+ * A 3 x 3 magic square is a 3 x 3 grid filled with distinct numbers from 1 to 9 such that each row,
+ * column, and both diagonals all have the same sum.
+ * <p>
+ * Given a row x col grid of integers, how many 3 x 3 magic square subgrids are there?
+ * <p>
+ * Note: while a magic square can only contain numbers from 1 to 9, grid may contain numbers up to 15.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: grid = [[4,3,8,4],[9,5,1,9],[2,7,6,2]]
+ * Output: 1
+ * Explanation:
+ * The following subgrid is a 3 x 3 magic square:
+ * <p>
+ * while this one is not:
+ * <p>
+ * In total, there is only one magic square inside the given grid.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: grid = [[8]]
+ * Output: 0
+ * <p>
+ * Constraints:
+ * <p>
+ * - row == grid.length
+ * <p>
+ * - col == grid[i].length
+ * <p>
+ * - 1 <= row, col <= 10
+ * <p>
+ * - 0 <= grid[i][j] <= 15
+ */
+public class _840 {
+
+    public int numMagicSquaresInside(int[][] grid) {
+        int len = 3;
+        int sum = 15;
+        boolean match = true;
+        int rowSum = 0;
+        int colSum = 0;
+        int digonalSum1 = 0;
+        int digonalSum2 = 0;
+        int xorNum = 0;
+        int ans = 0;
+        for (int row = 0; row < grid.length - 2; row++) {
+            for (int col = 0; col < grid[row].length - 2; col++) {
+
+                if (grid[row + 1][col + 1] != (len * len + 1) >> 1) {
+                    continue;
+                }
+
+                digonalSum1 = 0;
+                digonalSum2 = 0;
+                xorNum = 0;
+
+                // start check
+                for (int i = 0; i < len; i++) {
+
+                    // check row sum
+                    rowSum = 0;
+                    for (int j = 0; j < len; j++) {
+                        rowSum += grid[row + i][col + j];
+                    }
+                    if (rowSum != sum) {
+                        match = false;
+                        break;
+                    }
+
+                    // check col sum
+                    colSum = 0;
+                    for (int j = 0; j < len; j++) {
+                        colSum += grid[row + j][col + i];
+                    }
+                    if (colSum != sum) {
+                        match = false;
+                        break;
+                    }
+
+                    digonalSum1 += grid[row + i][col + i];
+                    digonalSum2 += grid[row + i][col + len - i - 1];
+                }
+
+
+                // check diagonals and num
+                if (!match || digonalSum1 != sum || digonalSum2 != sum) {
+                    continue;
+                }
+
+                for (int r = 0; r < len; r++) {
+                    for (int l = 0; l < len; l++) {
+                        xorNum |=  (1 << grid[row + r][col + l]);
+                    }
+                }
+
+                if (xorNum == (1 << (len * len + 1)) - 2) {
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
