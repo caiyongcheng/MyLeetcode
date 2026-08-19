388199f753e6c1ecc27cd23e3545e19cbec8f8c4
caiyongcheng <hercules.cyc@gmail.com>
2026-08-19 11:28:37 +0800

    feat<medium>: question of the day

---
+++ src/main/java/letcode/normal/medium/_1386.java
@@ -0,0 +1,100 @@
+package letcode.normal.medium;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 1386. Cinema Seat Allocation
+ * Difficulty: Medium
+ * Link: https://leetcode.cn/problems/cinema-seat-allocation/
+ * <p>
+ * A cinema has n rows of seats, numbered from 1 to n . Each row has 10 seats, numbered from 1 to 10.
+ * <p>
+ * You are given a 2D integer array reservedSeats , where reservedSeats[i] = [row i , seat i ] means
+ * that seat seat i in row row i is already reserved.
+ * <p>
+ * A four-person group must be assigned to four seats in the same row. The group can be seated in one
+ * of the following seat blocks:
+ * <p>
+ * - seats 2, 3, 4, 5
+ * <p>
+ * - seats 4, 5, 6, 7
+ * <p>
+ * - seats 6, 7, 8, 9
+ * <p>
+ * A block can be used only if none of its seats are reserved. Each seat can be assigned to at most one
+ * group.
+ * <p>
+ * Return an integer denoting the maximum number of four-person groups that can be assigned.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 3, reservedSeats = [[1,2],[1,3],[1,8],[2,6],[3,1],[3,10]]
+ * Output: 4
+ * Explanation: The figure above shows an optimal allocation of four groups. Seats marked in blue are
+ * already reserved, and each set of four contiguous seats marked in orange is assigned to one group.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: n = 2, reservedSeats = [[2,1],[1,8],[2,6]]
+ * Output: 2
+ * <p>
+ * Example 3:
+ * <p>
+ * Input: n = 4, reservedSeats = [[4,3],[1,4],[4,6],[1,7]]
+ * Output: 4
+ * <p>
+ * Constraints:
+ * <p>
+ * - 1 <= n <= 10 9
+ * <p>
+ * - 1 <= reservedSeats.length <= min(10 * n, 10 4 )
+ * <p>
+ * - reservedSeats[i] == [row i , seat i ]
+ * <p>
+ * - 1 <= row i <= n
+ * <p>
+ * - 1 <= seat i <= 10
+ * <p>
+ * - All reservedSeats[i] are distinct.
+ */
+public class _1386 {
+
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+
+        // because n between 1 and 10 ^ 9, so use sort replace map
+        Arrays.sort(reservedSeats, Comparator.comparingInt(a -> a[0]));
+
+        // The ideal outcome
+        int ans = n << 1;
+
+        int curRow = reservedSeats[0][0];
+        int mask = 0;
+        for (int[] reservedSeat : reservedSeats) {
+            if (reservedSeat[0] != curRow) {
+                ans -= lost(mask);
+                curRow = reservedSeat[0];
+                mask = 0;
+            }
+            mask |= 1 << reservedSeat[1];
+        }
+        ans -= lost(mask);
+
+        return ans;
+    }
+
+
+    private int lost(int mask) {
+
+        if (((mask >> 2) << 24) == 0) {
+            return 0;
+        }
+        if (((mask >> 2) << 28) == 0
+                || ((mask >> 4) << 28) == 0
+                || ((mask >> 6) << 28) == 0) {
+            return 1;
+        }
+        return 2;
+    }
+
+}
